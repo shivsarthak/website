890d666d564bd941bad3487974334919bcd68738
shivsarthak <shivsarthak34@gmail.com>
2020-09-05 00:23:55 +0530

    removed particle js

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,6 @@ import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import Typical from 'react-typical';
-import Particles from 'react-particles-js';
 
 const Header = () => {
   const [isDesktop, setIsDesktop] = useState(false);
@@ -19,62 +18,6 @@ const Header = () => {
   }, []);
   return (
     <section id="hero" className="jumbotron">
-      <div id="particles-js">
-        <Particles
-          params={{
-            particles: {
-              color: '#606060',
-              number: {
-                value: 160,
-                density: {
-                  enable: false,
-                },
-              },
-              size: {
-                value: 3,
-                random: true,
-                anim: {
-                  speed: 4,
-                  size_min: 0.3,
-                },
-              },
-              line_linked: {
-                enable: false,
-              },
-              move: {
-                random: true,
-                speed: 1,
-                direction: 'top',
-                out_mode: 'out',
-              },
-            },
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'bubble',
-                },
-                onclick: {
-                  enable: true,
-                  mode: 'repulse',
-                },
-              },
-              modes: {
-                bubble: {
-                  distance: 250,
-                  duration: 2,
-                  size: 0,
-                  opacity: 0,
-                },
-                repulse: {
-                  distance: 400,
-                  duration: 4,
-                },
-              },
-            },
-          }}
-        />
-      </div>
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
